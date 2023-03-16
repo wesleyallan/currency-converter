cf867d3b04e5729a8b5bf02114111963b8bcaa15
Wesley Silva <wesley.allansilva@gmail.com>
2023-03-15 22:59:49 -0300

    :bug: fix responsive
    Corrigido responsividade para mobile.

--- src/types/currency.ts
+++ src/types/currency.ts
@@ -16,7 +16,8 @@ export type TReducerAction =
   | { type: "SET_FROM"; payload: currency }
   | { type: "SET_TO"; payload: currency }
   | { type: "SET_FROM_VALUE"; payload: number }
-  | { type: "SET_EXCHANGE_RATE"; payload: number };
+  | { type: "SET_EXCHANGE_RATE"; payload: number }
+  | { type: "SWITCH_CURRENCIES" };
 
 export type TReducerAction2 = {
   type: "SET_FROM" | "SET_TO" | "SET_FROM_VALUE" | "SET_EXCHANGE_RATE";
